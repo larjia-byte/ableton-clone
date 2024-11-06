dd75ba5ee27db068a80012421f157acfcd530cd0
Yongzhen (Larry) Jiang <yongzhen.jiang@gmail.com>
2024-11-06 15:19:33 +1300

    Update media responsive for header menu

--- static/assets/js/app.js
+++ static/assets/js/app.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", function(e) {
-    console.log("Page has loaded!")
+
+    /* menu trigger */
+    const triggerBtn = document.getElementsByClassName("menu-trigger-btn");
+
+    console.log("triggerBtn")
+    console.log(triggerBtn)
+
+    triggerBtn[0].addEventListener("click", function(e) {
+
+        const triggerArrowDown = triggerBtn[0].getElementsByClassName("bi bi-caret-down-fill");
+        const triggerArrowUp = triggerBtn[0].getElementsByClassName("bi bi-caret-up-fill");
+
+        triggerArrowDown[0].style.display = triggerArrowDown[0].style.display === "inline-block" ? "none" : "inline-block"
+        triggerArrowUp[0].style.display = triggerArrowUp[0].style.display === "none" ? "inline-block" : "none"
+    })
 })
+
+
+
+
+
